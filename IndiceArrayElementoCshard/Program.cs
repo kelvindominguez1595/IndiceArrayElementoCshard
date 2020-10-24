6b80b4ebdd47f60c34360ff8fefacc207034f5dd
kelvindominguez1595 <kelvin.dominguez95@gmail.com>
2020-10-23 18:23:22 -0600

    Agregar archivos de proyecto.

---
+++ IndiceArrayElementoCshard/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IndiceArrayElementoCshard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Buscar información en un array");
+            string palabra = "";
+            string[] arrayString = new string[3];
+            arrayString[0] = "uno";
+            arrayString[1] = "dos";
+            arrayString[2] = "tres";
+            Console.WriteLine("Escriba la palabra a buscar");
+            palabra = Console.ReadLine();
+            for (int i = 0; i < arrayString.Length; i++)
+            {
+                if (arrayString[i] == palabra)
+                {
+                    Console.WriteLine("Posicion [" + i + "] --> " + arrayString[i]);
+                }
+            }
+        }
+    }
+}

d04ab0dc955ec1f59c357fca58df9d8923efa7a1
kelvindominguez1595 <kelvin.dominguez95@gmail.com>
2020-10-23 18:36:59 -0600

    Comentario de lineas

--- IndiceArrayElementoCshard/Program.cs
+++ IndiceArrayElementoCshard/Program.cs
@@ -7,17 +7,23 @@ namespace IndiceArrayElementoCshard
         static void Main(string[] args)
         {
             Console.WriteLine("Buscar información en un array");
-            string palabra = "";
-            string[] arrayString = new string[3];
+
+            string palabra = ""; // Declaramos una variable para buscar
+            string[] arrayString = new string[3]; // creamos un array
+            // llenamos de información el array
             arrayString[0] = "uno";
             arrayString[1] = "dos";
             arrayString[2] = "tres";
+            // pedimo que palabra buscar
             Console.WriteLine("Escriba la palabra a buscar");
             palabra = Console.ReadLine();
+            // utilizamos el ciclo for para saber el indice del elemento
             for (int i = 0; i < arrayString.Length; i++)
             {
+                // comparamos si se encuentra la palabra
                 if (arrayString[i] == palabra)
                 {
+                    // Mostramos el indice que en este caso seria la variable i + el contenido
                     Console.WriteLine("Posicion [" + i + "] --> " + arrayString[i]);
                 }
             }
